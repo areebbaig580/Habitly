51b10dfc52ee3f70f1e1baf8da303d9353f3d4e9
areebbaig580 <areebbaig580@gmail.com>
2026-04-14 13:40:25 +0530

    proggressSystem function

--- dashboard/dashbord.js
+++ dashboard/dashbord.js
@@ -1,23 +1,37 @@
 const back = document.querySelector('.symbol');
 const percent = document.querySelector('.complPercent');
 const percentDet = document.querySelector('.percentDet');
-const streak = document.querySelector('.streak');
+const streakEl = document.querySelector('.streak');
 const tracked = document.querySelector('.tracked');
 
 back.addEventListener('click', (evt) => {
     window.location.href = "../index.html";
 });
 
-function boxDetails(){
+function progressSystem() {
     let habitList = JSON.parse(localStorage.getItem('habitList')) || [];
-    let totHabits = habitList.length;
+    let streeks = JSON.parse(localStorage.getItem('streeks')) || 0;
+    let lastStreakDate = localStorage.getItem('lastStreakDate');
+    let today = new Date().toISOString().split('T')[0];
+    let yesterday = new Date(Date.now() - 86400000).toISOString().split('T')[0];
     let compHabits = habitList.filter(h => h.completed === true).length;
+    let totHabits = habitList.length;
+    let percentage = (compHabits / totHabits) * 100;
 
-    let percentage = (compHabits/totHabits)*100;
+    if (percentage >= 60) {
+        streeks = lastStreakDate === yesterday ? streeks + 1 : 1;
+        localStorage.setItem('streeks', JSON.stringify(streeks));
+        localStorage.setItem('lastStreakDate', today);
+    } else {
+        if (lastStreakDate !== today && lastStreakDate !== yesterday) {
+            streeks = 0;
+            localStorage.setItem('streeks', JSON.stringify(streeks));
+        }
+    };
 
     percent.innerHTML = `${percentage} %`;
     percentDet.innerHTML = `${compHabits} of ${totHabits} habits`
     tracked.innerHTML = totHabits;
-
+    streakEl.innerHTML = streeks;
 };
-boxDetails();
+progressSystem();
